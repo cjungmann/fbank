e527ab846fe96235d1878c4d0dd4bbe565ac4a32
Chuck Jungmann <chuckj60@gmail.com>
2018-12-10 17:27:54 -0600

    Moved reconcile procedure to Person.sql because its context is the person whose balances are being updated.

--- custom/Person.sql
+++ custom/Person.sql
@@ -96,4 +96,26 @@ BEGIN
    SELECT ROW_COUNT() AS deleted;
 END  $$
 
+-- ----------------------------------------------
+DROP PROCEDURE IF EXISTS App_Reconcile_Balances $$
+CREATE PROCEDURE App_Reconcile_Balances()
+BEGIN
+   UPDATE Person p
+          INNER JOIN (
+             SELECT id_person,
+                    SUM(CASE WHEN NOT(dorc)
+                             THEN amount
+                             ELSE 0
+                         END) AS debit,
+                    SUM(CASE WHEN dorc
+                             THEN amount
+                             ELSE 0
+                         END) AS credit
+               FROM TLine
+               GROUP BY id_person) s ON s.id_person = p.id
+      SET balance = s.debit - s.credit;
+
+      CALL App_Person_List();
+END $$
+
 DELIMITER ;
