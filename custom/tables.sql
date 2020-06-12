f0498b0fa1ae5d7028c30b557dcf09e71f55c955
Chuck Jungmann <chuckj60@gmail.com>
2020-06-12 15:35:18 -0500

    Added digit to currency numbers in Person and TLine tables.

--- custom/tables.sql
+++ custom/tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS Person
    id      INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
    name    VARCHAR(20),
    bday    DATE,
-   balance DECIMAL(5,2) DEFAULT 0
+   balance DECIMAL(6,2) DEFAULT 0
 );
 
 -- Transaction table
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS TLine
    id_taction   INT UNSIGNED NOT NULL,
    id_person    INT UNSIGNED NOT NULL,
    dorc         BOOLEAN,   -- Debit=0 Credit=1
-   amount       DECIMAL(5,2),
+   amount       DECIMAL(6,2),
 
    INDEX(id_taction),
    INDEX(id_person)
